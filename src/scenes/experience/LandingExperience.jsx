d89139e445978ad9233b9ece7b68b9a02fcd5942
josh1506 <jmjabor@gmail.com>
2023-07-07 10:40:40 +0800

    [Misc][Joshua] Add 3D character in hero section and add landing navbar

---
+++ src/scenes/experience/LandingExperience.jsx
@@ -0,0 +1,14 @@
+import {Canvas} from "@react-three/fiber";
+
+import LandingWorld from "../worlds/LandingWorld/index.jsx";
+
+
+const LandingExperience = () => {
+    return (
+        <Canvas>
+            <LandingWorld/>
+        </Canvas>
+    )
+}
+
+export default LandingExperience
