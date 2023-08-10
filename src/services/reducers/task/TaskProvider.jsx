aa45f3789e7b4128bb98cd79e87abdbddda295fd
josh1506 <jmjabor@gmail.com>
2023-08-10 23:49:44 +1200

    [Misc][Joshua] Enhance Code Structure and Readability

---
+++ src/services/reducers/task/TaskProvider.jsx
@@ -0,0 +1,12 @@
+import ListTaskProvider from "./TaskListProvider.jsx";
+import ModifyTaskProvider from "./TaskModifyProvider.jsx";
+
+const TaskProvider = ({children}) => {
+    return (
+        <ListTaskProvider>
+            <ModifyTaskProvider>
+                {children}
+            </ModifyTaskProvider>
+        </ListTaskProvider>
+    )
+}
